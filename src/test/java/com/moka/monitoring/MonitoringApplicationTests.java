6f5d084a0c478b55806598dd6143cfa39320302d
DESKTOP-13USF0H\USER <icemokacat@gmail.com>
2025-09-21 00:18:43 +0900

    init

---
+++ src/test/java/com/moka/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moka.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
